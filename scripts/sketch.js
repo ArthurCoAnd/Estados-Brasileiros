e61aff7584a426b2ef5fbedcee0b1002a2c39691
ArthurCoAnd <arthurcoand@gmail.com>
2019-01-24 10:08:49 -0200

    .

--- scripts/sketch.js
+++ scripts/sketch.js
@@ -31,6 +31,16 @@ var velocidade = 30;
 var tempo = 0;
 var tempoMaximo = 5 * velocidade;
 
+// Variaveis de situação
+var sit=1;
+var sit2tempoMax = 2.5*velocidade;
+var sit2tempo = 0;
+var sit2estado = true;
+var sit3tempoMax = 0.5*velocidade;
+var sit3tempo = 0;
+var sit3estado = true;
+
+
 var estado = new Array();
 
 // Variavel com Nome dos Estados Em Ordem Alfabética
@@ -94,52 +104,119 @@ var estadosSigla=[
 ];
 
 function preload(){
+	// Carregar na Memória Musica
 	musica = loadSound('audios/brasileirinho8bit.mp3');
+	// Carregar na Memória audio de Erro
+	erro = loadSound('audios/erro.mp3');
+	// Carregar na Memória audio de Acerto
+	acerto = loadSound('audios/acerto.mp3');
+	// Carregar imagens dos estados
 	for(var i=0; i<26; i++){
 		estado[i] = loadImage("imagens/"+estadosSigla[i]+".png");
 	}
 }
 
 function setup(){
+	// Configurações de Exibiçao
 	createCanvas(l, a);
 	frameRate(velocidade);
+	// Configurações de Imagens
 	imageMode(CENTER);
+	// Configurações de Foramas
 	rectMode(CENTER);
 	noStroke();
+	// Configuralções de Audio
 	musica.setVolume(0.1);
 	musica.setLoop(true);
 	musica.play();
+	erro.setVolume(0.25);
+	erro.setLoop(false);
+	acerto.setVolume(0.25);
+	acerto.setLoop(false);
+	// Primeira Execução de Mapa
 	novoMapa();
 }
 
 function draw(){
-	fill(corTexto);
-	rect(l/64*30, a/2, l/48, a/tempoMaximo*tempo);
-	tempo++;
+	// Jogo
+	if(sit==1){
+		fill(corTexto);
+		rect(l/64*30, a/2, l/48, a/tempoMaximo*tempo);
+		tempo++;
+	}
+	// Erro de Resposta
+	else if(sit==2){
+		if(sit2tempo%25==0){
+			if(sit2estado){
+				background(255,0,0);
+				sit2estado = !sit2estado;
+			}
+			else{
+				background(75);
+				sit2estado = !sit2estado;
+			}
+		}
+		if(sit2tempo>=sit2tempoMax){
+			novoMapa();
+			sit=1;
+		}
+		sit2tempo++;
+	}
+	// Acerto de Respsota
+	else if(sit==3){
+		if(sit3tempo%5==0){
+			if(sit3estado){
+				background(0,255,0);
+				sit3estado = !sit3estado;
+			}
+			else{
+				background(75);
+				sit3estado = !sit3estado;
+			}
+		}
+		if(sit3tempo>=sit3tempoMax){
+			novoMapa();
+			sit=1;
+		}
+		sit3tempo++;
+	}
 }
 
 function keyPressed(){
-	// Verificar se a tecla apertada é valida
-	if(key >= '1' && key <= '4'){
-		// Verificar se a resposta foi digitada a tempo
-			// Verificar se a respsota foi correta
-		if(tempo<tempoMaximo && key == respPos+1){
-			pontos = pontos + tempoMaximo - tempo;
-		}
-		else{
-			// Verificar se a pontuação atual foi maior que a maior pontuação
-			if(pontos>pontosMax){
-				pontosMax=pontos;
+	// Verificar se esta na situação 1
+	if(sit==1){
+		// Verificar se a tecla apertada é valida
+		if(key >= '1' && key <= '4'){
+			// Verificar se a resposta foi digitada a tempo
+				// Verificar se musica.play();a respsota foi correta
+			if(tempo<tempoMaximo && key == respPos+1){
+				pontos = pontos + tempoMaximo - tempo;
+				// Mudando para situação de acerto
+				sit=3;
+				sit3tempo=0;
+				sit3estado=true;
+				acerto.play();
 			}
-			pontos=0;
+			else{
+				// Verificar se a pontuação atual foi maior que a maior pontuação
+				if(pontos>pontosMax){
+					pontosMax=pontos;
+				}
+				pontos=0;
+				// Mudando para situação de erro
+				sit=2;
+				sit2tempo=0;
+				sit3estado=true;
+				erro.play();
+			}
 		}
-		tempo=0;
-		novoMapa();
 	}
 }
 
 function novoMapa(){
+	// "Limpar a Tela"
 	background(75);
+	tempo=0;
 
 	// Desenhar Placar
 	fill(corTexto);
